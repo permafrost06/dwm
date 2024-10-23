6cf740e156f4c38d89dbbc39ff4d5810e2353db7
Zahin Azmayeen <zahin06@gmail.com>
2024-10-23 16:19:06 +0600

    add attachbelow patch

--- config.h
+++ config.h
@@ -70,6 +70,7 @@ static const Rule rules[] = {
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static int attachbelow = 1;
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
